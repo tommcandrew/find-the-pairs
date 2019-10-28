a2ea477e4df6fc6c085ff311a5f6a09666390893
Thomas McAndrew <tommcandrew@hotmail.com>
2019-10-28 12:36:26 +0000

    add game over check

--- src/App.js
+++ src/App.js
@@ -21,12 +21,20 @@ class App extends React.Component {
             checkedCells[i].classList.add('turned')
             checkedCells[i].classList.remove('checking')
           }
+          if (document.getElementsByClassName('turned').length === 16) {
+            alert('game over')
+          }
         } else {
           console.log('different')
           const checkedCells = document.getElementsByClassName('checking')
-          for (let i = 1; i >= 0; i--) {
-            checkedCells[i].classList.remove('checking')
-          }
+          setTimeout(() => {
+
+            for (let i = 1; i >= 0; i--) {
+              checkedCells[i].classList.remove('checking')
+            }
+
+          }, 1000);
+
         }
         this.setState(() => ({prevCell: ''}))
       }
@@ -54,12 +62,3 @@ class App extends React.Component {
 }
 
 export default App;
-
-
-//make a 4 x 4 grid containing cells with a class for its picture (two of each)
-//add click listener that prints the class and adds turned class
-//create function that saves clicked class in state and then compares with next one and alerts whether same or not
-//make function clear state after two clicks
-//if classes are the same, keep turned class on both, if not, remove from both
-//add css rule that distinguishes turned from unturned (color change)
-//add code that checks if all cards have been turned and ends game if so
