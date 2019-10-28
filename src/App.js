88511c6eb21de54d62b20c84b3ee52d406bf8a7c
Thomas McAndrew <tommcandrew@hotmail.com>
2019-10-28 20:54:27 +0000

    generate grid from array of only 8

--- src/App.js
+++ src/App.js
@@ -2,14 +2,12 @@ import React from 'react';
 import Card from './Card'
 import './App.css';
 
-const foods = ['apple', 'cookies', 'pizza', 'coffee', 'cookies', 'cheese', 'chicken', 'cheese', 'coffee', 'spaghetti', 'apple', 'juice', 'chicken', 'juice', 'spaghetti', 'pizza']
-const mixedFoods = []
+const foods = ['apple', 'cookies', 'pizza', 'coffee', 'cheese', 'chicken', 'spaghetti', 'juice']
 
 class App extends React.Component {
 
   state = {
     prevCard: '',
-    mixedFoods: [],
     foods1: [],
     foods2: [],
     foods3: [],
@@ -17,25 +15,21 @@ class App extends React.Component {
   }
 
   componentDidMount() {
-    this.randomiseFoods()
-    this.setState(() => ({mixedFoods}))
-
-    const foods1 = mixedFoods.slice(0, 4)
-    const foods2 = mixedFoods.slice(4, 8)
-    const foods3 = mixedFoods.slice(8, 12)
-    const foods4 = mixedFoods.slice(12)
-
+    const mixedArray = this.randomiseFoods()
+    const foods1 = mixedArray.slice(0, 4)
+    const foods2 = mixedArray.slice(4, 8)
+    const foods3 = mixedArray.slice(8, 12)
+    const foods4 = mixedArray.slice(12)
     this.setState(() => ({foods1, foods2, foods3, foods4}))
   }
 
   randomiseFoods = () => {
-    if (foods.length === 0) {
-      return
+    let mixedArray = [...foods, ...foods]
+    for (let i = mixedArray.length - 1; i > 0; i--) {
+      const randomItem = Math.floor(Math.random() * (i + 1));
+      [mixedArray[i], mixedArray[randomItem]] = [mixedArray[randomItem], mixedArray[i]];
     }
-    const randomNum = Math.floor(Math.random() * foods.length)
-    const randomFood = foods.splice(randomNum, 1)[0]
-    mixedFoods.push(randomFood)
-    this.randomiseFoods()
+    return mixedArray
   }
 
   handleClick = (e) => {
@@ -47,7 +41,6 @@ class App extends React.Component {
         this.setState(() => ({prevCard: symbol}))
       } else {
         if (symbol === this.state.prevCard) {
-          console.log('same')
           const checkedCards = document.getElementsByClassName('checking')
           setTimeout(() => {
             for (let i = 1; i >= 0; i--) {
@@ -58,18 +51,13 @@ class App extends React.Component {
               alert('game over')
             }
           }, 500);
-
         } else {
-          console.log('different')
           const checkedCards = document.getElementsByClassName('checking')
           setTimeout(() => {
-
             for (let i = 1; i >= 0; i--) {
               checkedCards[i].classList.remove('checking')
             }
-
           }, 500);
-
         }
         this.setState(() => ({prevCard: ''}))
       }
