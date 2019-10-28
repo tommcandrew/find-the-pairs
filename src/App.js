a802f1161864ec19e50e51a2f6ff6c919aa992f4
Thomas McAndrew <tommcandrew@hotmail.com>
2019-10-28 11:20:20 +0000

    write plan in comments

--- src/App.js
+++ src/App.js
@@ -10,3 +10,12 @@ function App() {
 }
 
 export default App;
+
+
+//make a 4 x 4 grid containing cells with a class for its picture (two of each)
+//add click listener that prints the class and adds turned class
+//create function that saves clicked class in state and then compares with next one and alerts whether same or not
+//make function clear state after two clicks
+//if classes are the same, keep turned class on both, if not, remove from both
+//add css rule that distinguishes turned from unturned (color change)
+//add code that checks if all cards have been turned and ends game if so
