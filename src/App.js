7ff1135f2cd9b00226b91aca214b3aebf44b1b84
Thomas McAndrew <tommcandrew@hotmail.com>
2019-10-28 12:28:04 +0000

    add all logic

--- src/App.js
+++ src/App.js
@@ -1,12 +1,56 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div>
+class App extends React.Component {
 
-    </div>
-  );
+  state = {
+    prevCell: ''
+  }
+
+  handleClick = (e) => {
+    if (e.target.classList.contains('cell') && e.target.classList.contains('checking') === false && e.target.classList.contains('turned') === false) {
+      e.target.classList.add('checking')
+      const symbol = e.target.classList[1]
+      if (this.state.prevCell === '') {
+        this.setState(() => ({prevCell: symbol}))
+      } else {
+        if (symbol === this.state.prevCell) {
+          console.log('same')
+          const checkedCells = document.getElementsByClassName('checking')
+          for (let i = 1; i >= 0; i--) {
+            checkedCells[i].classList.add('turned')
+            checkedCells[i].classList.remove('checking')
+          }
+        } else {
+          console.log('different')
+          const checkedCells = document.getElementsByClassName('checking')
+          for (let i = 1; i >= 0; i--) {
+            checkedCells[i].classList.remove('checking')
+          }
+        }
+        this.setState(() => ({prevCell: ''}))
+      }
+    }
+  }
+
+  render() {
+
+    return (
+
+      <div className="grid-wrapper" onClick={this.handleClick}>
+
+        <div className="row"><div className="cell apple"></div><div className="cell cake"></div><div className="cell pizza"></div><div className="cell coffee"></div></div>
+        <div className="row"><div className="cell cake"></div><div className="cell cheese"></div><div className="cell chicken"></div><div className="cell cheese"></div></div>
+        <div className="row"><div className="cell coffee"></div><div className="cell spaghetti"></div><div className="cell apple"></div><div className="cell orange-juice"></div></div>
+        <div className="row"><div className="cell chicken"></div><div className="cell orange-juice"></div><div className="cell spaghetti"></div><div className="cell pizza"></div></div>
+
+
+      </div>
+
+    );
+
+  }
+
 }
 
 export default App;
