c2861743c4233a7b5f62f93ac88253277107febc
Thomas McAndrew <tommcandrew@hotmail.com>
2019-10-30 01:08:53 +0000

    add form to menu modal for new user

--- src/components/MainMenu.js
+++ src/components/MainMenu.js
@@ -2,21 +2,46 @@ import React from 'react'
 import Header from './Header'
 import './styles/MainMenu.css'
 
-const MainMenu = (props) => {
-    return (
-        <div className="menu-wrapper">
-            <div className="menu-content">
-                <Header />
-                <p className="choose-para">Choose player:</p>
-                <ul className="user-list" onClick={props.handleChoose}>
-                    <li>NEW PLAYER</li>
-                    <li>John</li>
-                    <li>Sam</li>
-                    <li>Barry</li>
-                </ul>
+class MainMenu extends React.Component {
+
+    state = {
+        showForm: false
+    }
+
+    showForm = () => {
+        this.setState(() => ({showForm: true}))
+    }
+
+    render() {
+
+        return (
+            <div className="menu-wrapper">
+                <div className="menu-content">
+                    <Header />
+                    {!this.state.showForm &&
+                        <div>
+                            <p className="choose-para">Choose player:</p>
+                            <p className="new-player" onClick={this.showForm}>NEW PLAYER</p>
+                            <ul className="user-list" onClick={this.props.handleChoose}>
+                                <li>John</li>
+                                <li>Sam</li>
+                                <li>Barry</li>
+                            </ul>
+                        </div>
+                    }
+                    {this.state.showForm &&
+                        <div className="form-wrapper">
+                            <p className="form-label">Enter your name:</p>
+                            <input type="text" className="form-input" />
+                            <p className="play-button">LET'S PLAY!</p>
+                        </div>
+                    }
+                </div>
             </div>
-        </div>
-    )
+        )
+
+    }
+
 }
 
 export default MainMenu
